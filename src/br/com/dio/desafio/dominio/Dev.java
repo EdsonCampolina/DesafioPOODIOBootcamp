42e6f9b50545a00c8392009dfe9870991d1d79c6
EdsonCampolina <ejcsengsoft@gmail.com>
2022-02-24 02:31:22 -0300

    Adicionando Bootcamp e Dev

---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,40 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Dev {
+
+	private String nome;
+	private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+	private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+	public void inscreverBootcamp(Bootcamp bootcamp) {
+		
+	}
+	
+	public void progredir() {
+		
+	}
+	
+	public void calcularXp() {
+		
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Set<Conteudo> getConteudosInscritos() {
+		return conteudosInscritos;
+	}
+
+	public Set<Conteudo> getConteudosConcluidos() {
+		return conteudosConcluidos;
+	}
+
+}
