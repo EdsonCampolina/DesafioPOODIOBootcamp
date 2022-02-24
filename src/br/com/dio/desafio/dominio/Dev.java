4eb709d85318828c2b1823fb8a47d80f90a8e3fc
EdsonCampolina <ejcsengsoft@gmail.com>
2022-02-24 02:42:55 -0300

    Adicionando metodos em Dev

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,6 +1,8 @@
 package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -10,17 +12,42 @@ public class Dev {
 	private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
 	public void inscreverBootcamp(Bootcamp bootcamp) {
-		
+		this.conteudosInscritos.addAll(bootcamp.getConteudos());
+		bootcamp.getDevsInscritos().add(this);
 	}
-	
+
 	public void progredir() {
-		
+		Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+		if (conteudo.isPresent()) {
+			this.conteudosConcluidos.add(conteudo.get());
+			this.conteudosInscritos.remove(conteudo.get());
+		} else {
+			System.err.println("Você não está matriculado em nenhum conteúdo!");
+		}
 	}
-	
-	public void calcularXp() {
-		
+
+	public double calcularXp() {
+		return this.conteudosConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXp()).sum();
 	}
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(conteudosConcluidos, conteudosInscritos, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dev other = (Dev) obj;
+		return Objects.equals(conteudosConcluidos, other.conteudosConcluidos)
+				&& Objects.equals(conteudosInscritos, other.conteudosInscritos) && Objects.equals(nome, other.nome);
+	}
+
 	public String getNome() {
 		return nome;
 	}
